bf5067208f81120af1c2bb07e8b86fbdbac5ac38
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-12-10 18:26:56 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskayIV.Sprint4.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BreslavskayIV.Sprint4.Task1.V11.Lib;
+
+namespace Tyuiu.BreslavskayIV.Sprint4.Task1.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsarray = { 2, 6, 5, 5, 1, 5, 6, 5, 5, 5, 1, 5, 3, 6, 6, 1, 6 };
+            int res = ds.Calculate(numsarray);
+            int wait = 32;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BreslavskayIV.Sprint4.Task1.V11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BreslavskayIV.Sprint4.Task1.V11.Lib;
+
+namespace Tyuiu.BreslavskayIV.Sprint4.Task1.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнила: Бреславская И. В. | ПКТб-23-2";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                              *");
+            Console.WriteLine("* Задание #1                                                               *");
+            Console.WriteLine("* Вариант #11                                                              *");
+            Console.WriteLine("* Выполнила: Бреславская И. В. | ПКТб-23-2                                 *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 17 элементов заполненный          *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму четных    *");
+            Console.WriteLine("* элементов массива.                                                       *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            DataService ds = new DataService();
+            int len;
+            Console.Write("*Введите количество элементов массива                                          ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numar = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write("*Введите значение " + i + " элемента массива                                     ");
+                numar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numar[i]+ "\t");
+              
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int res = ds.Calculate(numar);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
